88dc389345aed8139d500d6098245df18630cd05
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-16 12:09:52 -0400

    ADD Daily Report for AOL Students Program

---
+++ Daily Report for AOL Students/Daily Report for AOL Students/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Daily_Report_for_AOL_Students
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Academy of Learning Career College\nStudent Daily Report");
+            Console.WriteLine("\nWhat course are you in?");
+            string studCourse = Console.ReadLine();
+            
+            Console.WriteLine("What page number?");
+            string pgNumber = Console.ReadLine();
+            byte pgNumberByte = Convert.ToByte(pgNumber);
+            
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            string needHelp = Console.ReadLine();
+            bool needHelpBool = Convert.ToBoolean(needHelp);
+            
+            
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+            
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific");
+            string feedback = Console.ReadLine();
+            
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            decimal hoursStudiedDec = Convert.ToDecimal(hoursStudied);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
