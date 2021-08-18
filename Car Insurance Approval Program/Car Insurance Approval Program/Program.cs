f252b1bfb5b36f1150da77582556efacc8ec9617
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-18 08:19:43 -0400

    ADD_Car_Insurance_Approval_Program

---
+++ Car Insurance Approval Program/Car Insurance Approval Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Car_Insurance_Approval_Program
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Car Insurance Approval \n\nPress Enter to Begin");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string num1 = Console.ReadLine();
+            int age = Convert.ToInt32(num1);
+
+            Console.WriteLine("How many DUIs do you have?");
+            string num2 = Console.ReadLine();
+            int dui = Convert.ToInt32(num2); 
+
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string num3 = Console.ReadLine();
+            int tickets = Convert.ToInt32(num3);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == 0 && tickets <= 3);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
