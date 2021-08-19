8a3882c2a16024a8f3cdb71c569cc1094e1c5476
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-19 10:08:09 -0400

    ADD_Strings_Program

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string one = "hello,";
+            string two = " how are";
+            string three = " you?";
+
+            Console.WriteLine(one + two + three);
+            Console.ReadLine();
+
+            one = one.ToUpper();
+
+            Console.WriteLine(one);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hello, how are you?");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
