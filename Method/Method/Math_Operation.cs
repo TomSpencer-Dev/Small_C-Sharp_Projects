4c3bb1c7f9dbf27bfb657c8b6d748a9c7c626a77
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-24 11:18:14 -0400

    ADD_Math_Operations

---
+++ Method/Method/Math_Operation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Math_Operation
+    {
+        public void Divide(int divide)
+        {
+            int result = divide / 3;
+            Console.WriteLine(divide + " divided by three is: " + result);
+        }
+         
+
+        public void Multiplication(int multiply)
+        {
+
+            int result = multiply * 3;
+            Console.WriteLine(multiply + " multiplied by three is: " + result);
+        }
+        
+
+        public void Adding(int add)
+        {
+
+            int result = add + 3;
+            Console.WriteLine(add + " plus three is: " + result);
+        }
+       
+    }
+
+    
+}
---
+++ Method/Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Math Operations!" +
+                "\n\nYou will choose a number and we will divide by and multiply it by 3. Will will also add 3 to it." +
+                "\n\nPlease choose a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            var math = new Math_Operation();
+            math.Divide(input);
+            Console.WriteLine("\n Press Enter");
+            Console.ReadLine();
+            math.Multiplication(input);
+            Console.WriteLine("\n Press Enter");
+            Console.ReadLine();
+            math.Adding(input);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
