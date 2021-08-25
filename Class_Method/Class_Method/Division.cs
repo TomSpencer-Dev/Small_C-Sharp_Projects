fef771415b98c6c68d12a51e4420d22c3baf03de
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-25 13:10:25 -0400

    ADD_Class_Method

---
+++ Class_Method/Class_Method/Division.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    public class Division
+    {
+        public void divide(int input)
+        {
+            int result = input / 2;
+            Console.WriteLine(input + "  divided by two equals: " + result);
+
+        }
+
+        public void getValues(out int x, out int y)
+        {
+            Console.WriteLine("Enter a number");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            return sum;
+        }
+    }
+
+    public static class Math
+    {
+
+        public static int multiply(int input2)
+        {
+            int result2 = input2 * 2;
+            return result2;
+
+        }
+    }       
+      
+
+}
---
+++ Class_Method/Class_Method/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole even number to divide by 2");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            var math = new Division();
+            math.divide(input);
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+
+           
+
+            int a, b;
+
+            var math1 = new Division();
+            math1.getValues(out a, out b);
+
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+
+            var math2 = new Division();
+
+            int sum1 = math2.Add(1, 2);
+            Console.WriteLine("sum of the two " + "integer value : " + sum1);
+
+            int sum2 = math2.Add(1, 2, 3);
+            Console.WriteLine("sum of the three " + "integer value : " + sum2);
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a whole number to multiply by 2");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number multiplied by 2 is: " + Math.multiply(input2));
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
