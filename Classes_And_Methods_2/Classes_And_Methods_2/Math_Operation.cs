af1a1105b04e682a9877fb4b323e6da317d30550
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-25 10:57:29 -0400

    ADD_Classes_And_Methods_2

---
+++ Classes_And_Methods_2/Classes_And_Methods_2/Math_Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_2
+{
+    class Math_Operation
+    {
+        public int Passing_In_Two_Integers(int num1, int num2 = 0)
+        {
+            int sum = num1 + num2 + 5;
+            
+            return sum;
+        }
+
+    }
+}
---
+++ Classes_And_Methods_2/Classes_And_Methods_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Math Operations!" +
+                "\n\nPlease enter two numbers one at a time. You don't need to enter anything for the second number.");
+
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int result;
+            var math = new Math_Operation();
+            try
+            {
+                
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                result = math.Passing_In_Two_Integers(input1, input2);
+            }
+
+            catch
+            {
+                result = math.Passing_In_Two_Integers(input1);
+            }
+
+            Console.WriteLine("Your entry plus 5 is: " + result);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
