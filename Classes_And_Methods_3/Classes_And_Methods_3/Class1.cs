534fca50d4eb6a29de3ef5f80dad40d300213d8f
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-25 11:34:32 -0400

    ADD_Classes_And_Methods_3

---
+++ Classes_And_Methods_3/Classes_And_Methods_3/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_3
+{
+    public class Math_Operation
+    {
+        public void Passing_In_Two_Numbers(int num1, int num2)
+        { 
+
+            int result = num1 + 5;
+            Console.WriteLine(num2);
+
+        }
+
+        
+    }
+}
---
+++ Classes_And_Methods_3/Classes_And_Methods_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Math Operations!" +
+                "\n\nPlease enter two numbers one at a time.");
+
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            var math = new Math_Operation();
+            math.Passing_In_Two_Numbers(input1, input2);
+
+
+
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
