f9f7768f7428b9e58ac69b6565502aa5647a7ae0
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-08-25 09:23:53 -0400

    ADD_Classes_And_Methods_1

---
+++ Classes_And_Methods_1/Classes_And_Methods_1/Math_Operation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_1
+{
+    public class Math_Operation
+    {
+        public void Passing_in_an_Integer(int add)
+        { 
+        int result = add + 5;
+        Console.WriteLine(add + " plus five is: " + result);
+        }
+
+        public void Passing_in_an_Decimal(decimal subtract)
+        {
+            decimal result = subtract - 5;
+            Console.WriteLine(subtract + " minus five is: " + Convert.ToInt32(result));
+        }
+
+        public void Passing_in_a_String(string multiply)
+        {   
+            int result = Convert.ToInt32(multiply) * 5;
+            Console.WriteLine(multiply + " times five is: " + result);
+        }
+    }
+}
---
+++ Classes_And_Methods_1/Classes_And_Methods_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Math Operations!" +
+                "\n\nPlease enter a number to execute math operations on.");
+                
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+
+            var math = new Math_Operation();
+            math.Passing_in_an_Integer(input);
+            Console.WriteLine("\nPlease enter a number greater than five to execute math operations on.");
+            decimal input1 = Convert.ToDecimal(Console.ReadLine());
+
+            var math1 = new Math_Operation();
+            math1.Passing_in_an_Decimal(input1);
+            Console.WriteLine("\n Please enter a number to multiply by five");
+            string input2 = Console.ReadLine();
+
+            var math2 = new Math_Operation();
+            math2.Passing_in_a_String(input2);
+            Console.ReadLine();
+        }
+    }
+}
