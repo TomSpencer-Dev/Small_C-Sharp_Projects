0c26bd95626eda5cf8b3eaae8c4fe74865e0631a
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-07 11:35:57 -0400

    ADD Operator_Overloading

---
+++ Operator_Overloading/Operator_Overloading/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public static bool operator==(Employee employee, Employee employee1)
+        {
+            return employee.id == employee1.id;
+
+        }
+
+        public static bool operator!=(Employee employee, Employee employee1)
+        {
+            return employee.id != employee1.id;
+
+        }
+    }
+}
---
+++ Operator_Overloading/Operator_Overloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+            employee.id = 1;
+            var employee2 = new Employee();
+            employee2.id = 2;
+
+            Console.WriteLine(employee == employee2);
+            Console.ReadLine();
+
+        }
+    }
+}
