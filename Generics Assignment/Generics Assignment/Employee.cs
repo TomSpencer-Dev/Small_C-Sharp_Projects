e567696342dd71e586356142360ff685be75cf62
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-08 10:33:23 -0400

    Add Generics

---
+++ Generics Assignment/Generics Assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    public class Employee<T>
+    {
+
+        public List<T> things = new List<T>();
+        
+        
+       
+    }
+}
---
+++ Generics Assignment/Generics Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> name = new Employee<string>();
+            List<string> Things = new List<string>(){ "Bob", "Carol"};
+
+            Employee<int> ID = new Employee<int>();
+            List<int> Things = new List<int>() { 1, 2 };
+
+          
+            
+        }
+    }
+}
