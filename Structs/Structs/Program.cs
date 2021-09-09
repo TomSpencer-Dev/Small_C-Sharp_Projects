f789752a58447190bf3a606bd567417bc658fe3e
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-09 11:20:04 -0400

    ADD Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount;
+        }
+        static void Main(string[] args)
+        {
+            Number number;
+
+            number.Amount = 4;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }  
+        
+    }
+}
