7db8f84738f6a6b7d174ce0083a55d8ea4e20bec
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-17 11:41:49 -0400

    ADD_File_I/O_Assignment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,8 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
-            string text = "Here is some text.";
+            Console.WriteLine("Please enter a number");
+            string text = (Console.ReadLine());
             File.WriteAllText(@"C:\Users\Thomas\Desktop\Coding\Coding\Web Pages\GitHub\Small_C-Sharp_Projects\log.txt", text);
             
         }
