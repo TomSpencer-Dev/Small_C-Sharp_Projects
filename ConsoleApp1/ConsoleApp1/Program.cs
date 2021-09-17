41ce40a4a88eaeb0528b23e0f0cf9c3b8469c5c0
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-17 09:54:05 -0400

    ADD_TD_Assignment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Here is some text.";
+            File.WriteAllText(@"C:\Users\Thomas\Desktop\Coding\Coding\Web Pages\GitHub\Small_C-Sharp_Projects\log.txt", text);
+            
+        }
+    }
+}
---
+++ DT_Assignment/DT_Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DT_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current Date and Time is: ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now + "\n\nEnter a number");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            DateTime newTime = now.AddHours(X);
+            Console.WriteLine("It will be " + newTime + " " + X + " hours from now.");
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
