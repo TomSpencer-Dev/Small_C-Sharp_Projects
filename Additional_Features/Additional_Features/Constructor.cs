abe7e136eee6180691e7e0785cb5a7ec02b28e37
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-20 09:58:58 -0400

    ADD_ADDITIONAL_FEATURES

---
+++ Additional_Features/Additional_Features/Constructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    public class Constructor
+    {
+        public Constructor()
+        {
+
+        }
+
+        public Constructor(int Age)
+        {
+
+        }
+
+        public Constructor(int Age, string Name)
+        {
+
+        }
+
+        
+           
+    }
+}
---
+++ Additional_Features/Additional_Features/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Program
+    {
+        const int a = 10;
+
+        static void Main(string[] args)
+        {
+
+            const int b = 20;
+            const int c = b + a;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+            var name = "Tom";
+            var age = "36";
+
+            Constructor obj = new Constructor();
+            Constructor obj1 = new Constructor(12);
+            Constructor obj2 = new Constructor(36, "Tom");
+            
+
+
+        }
+    }
+}
