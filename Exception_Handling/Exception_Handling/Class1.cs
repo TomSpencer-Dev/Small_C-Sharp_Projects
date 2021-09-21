69b60ed85d4211692e1892e1682e7daf49ffc1a6
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-09-21 10:02:12 -0400

    ADD_Exception_Handling

---
+++ Exception_Handling/Exception_Handling/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    public class Class1
+    {
+        public static void myMethod()
+        {
+            Console.WriteLine("Please enter your age:");
+            int currentDate = DateTime.Now.Year;
+            int age = Convert.ToInt32(Console.ReadLine());
+            int DOB = currentDate - age;
+            if (age <= 0)
+            {
+                Console.WriteLine("Please enter a number greater than 0");
+                Class1.myMethod();
+                Console.ReadLine();
+                
+                
+
+            }
+
+            else
+            {
+                Console.WriteLine("You were born in " + DOB);
+                Console.ReadLine();
+            }
+        }
+}
+}
---
+++ Exception_Handling/Exception_Handling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+      
+        
+    
+        static void Main(string[] args)
+        {
+            try
+            {
+                Class1.myMethod();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured. Please contact your System Administrator");
+                Console.ReadLine();
+                return;
+            }
+
+
+
+
+
+        }
+    }
+}
