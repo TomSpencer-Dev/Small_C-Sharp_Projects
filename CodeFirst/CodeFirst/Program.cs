d0177e33c9f0441c38e7daf21db814ea3251bbe1
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-10-05 12:52:01 -0400

    ADD Entity Code-First

---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections;
+using System.Collections.Generic;
+using EF6Console;
+using CodeFirst;
+
+namespace EF6Console
+{ 
+
+   public class Program
+        {
+            public static void Main(string[] args)
+            {
+            
+                using (var ctx = new SchoolContext())
+                {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+
+               
+                
+            }
+            }
+        }
+}
---
+++ CodeFirst/CodeFirst/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using CodeFirst;
+
+namespace EF6Console
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
